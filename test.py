035e1ff32c26ca65496c00175073f9f5ab1ab1e3
bartcanc <bartekr2002@gmail.com>
2024-06-03 23:27:08 +0200

    minor fix

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import cv2
 import numpy as np
-from Crypto.PublicKey import RSA
-import os
 
 # -------------------------------------------------------------- random bits generating
 def calculateColor(px):
